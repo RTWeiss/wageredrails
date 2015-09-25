dd8a8dd93296429afae5b7c9c5e58a7cae9ebbaf
Alex Szabo <87szabo@gmail.com>
2015-09-25 12:23:31 -0700

    Makes AddLeagueIdColumnToGames migration also update the games league_id

--- db/migrate/20150925185240_add_league_id_column_to_games.rb
+++ db/migrate/20150925185240_add_league_id_column_to_games.rb
@@ -2,5 +2,9 @@ class AddLeagueIdColumnToGames < ActiveRecord::Migration
   def change
     add_column :games, :league_id, :integer
     add_foreign_key :games, :leagues, column: :league_id, primary_key: "id"
+    game = Game.where(league_id: nil)
+    game.each do |game|
+      game.update(league_id: game.home_team.league_id)
+    end
   end
 end
